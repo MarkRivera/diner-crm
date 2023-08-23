523fc936b3763f5fd550f8ca65b34046bce27c3c
Mark Rivera <Mrivera1991@gmail.com>
2023-08-22 20:28:17 -0400

    formatted files

--- src/payment-intent/dto/update-payment-intent.dto.ts
+++ src/payment-intent/dto/update-payment-intent.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePaymentIntentDto } from './create-payment-intent.dto';
 
-export class UpdatePaymentIntentDto extends PartialType(CreatePaymentIntentDto) {}
+export class UpdatePaymentIntentDto extends PartialType(
+  CreatePaymentIntentDto,
+) {}
